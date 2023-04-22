dce6077f19735f2b8caa8ed9df0f1dc31c3bf6c6
Bhaltair <gilliarool@mail.ru>
2023-04-22 11:51:50 +0300

    2 задача решена

---
+++ Task #2/Task#2.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+void XYRange(double x1, double y1, double x2, double y2, double z1, double z2)
+{
+    double result = Math.Sqrt((Math.Pow((x1 - x2), 2)) + (Math.Pow((y1 - y2), 2)+ (Math.Pow((z1 - z2), 2))));
+    Console.WriteLine(result);
+}
+Console.WriteLine("Введите число x1: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число x2: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число y1: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число y2: ");
+double y2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число z1: ");
+double z1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число z2: ");
+double z2 = double.Parse(Console.ReadLine());
+XYRange(x1, x2, y1, y2, z1, z2);
---
+++ Task #3/Task#3.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void PowNumbers(int number)
+{
+    for (int y = 1; y <= number; y++)
+    Console.Write(Math.Pow(y, 3) + ", ");
+
+}
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+PowNumbers(number);
